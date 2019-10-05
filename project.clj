0d1c593a5f3075393fcdc6dafcfd6b7749097f79
Christopher Mark Gore <cgore@cgore.com>
2019-10-05 15:52:02 -0500

    project.clj update

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject evolve "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.cgore/evolve "0.1.0-SNAPSHOT"
+  :description "A Clojure/ClojureScript library for Evolutionary Algorithms."
+  :url "https://github.com/cgore/clojure-evolve"
+  :scm {:name "git"
+        :url "https://github.com/cgore/clojure-evolve"}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
